e2122414c0fe3a2e4aa3b59c028bdd8876a28f4f
Skinkworks <skinkworks42@gmail.com>
2012-08-16 15:04:22 -0400

    Added eclipse files

---
+++ ChatServerJava/src/net/tigerclan/ChatServer/ChatServer.java
@@ -0,0 +1,13 @@
+package net.tigerclan.ChatServer;
+
+public class ChatServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
